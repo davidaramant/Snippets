37f929daa5ab0d7b96eee5875e832dcc2892022a
aramant <aramant@sep.com>
2012-01-26 13:27:08 -0500

    Initial checkin

---
+++ Solution/ConsoleRunner/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace ConsoleRunner
+{
+    sealed class Program
+    {
+        public static void RunSnippet( string[] args )
+        {
+
+        }
+
+        #region Helper methods
+
+        public static void Main( string[] args )
+        {
+            try
+            {
+                RunSnippet( args );
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( "---\nEXCEPTION:\n" + e + "\n" );
+            }
+            finally
+            {
+                Console.Write( "Press any key to continue..." );
+                Console.ReadKey();
+            }
+        }
+
+        private static void WL( object text, params object[] args )
+        {
+            if( text == null )
+            {
+                Console.WriteLine( "null" );
+            }
+            else
+            {
+                Console.WriteLine( text.ToString(), args );
+            }
+        }
+
+        private static void RL()
+        {
+            Console.ReadLine();
+        }
+
+        private static void Break()
+        {
+            System.Diagnostics.Debugger.Break();
+        }
+
+        class Timed : IDisposable
+        {
+            readonly System.Diagnostics.Stopwatch _watch;
+            readonly string _name = String.Empty;
+            readonly System.Threading.ThreadPriority _previous;
+
+
+            private Timed( string name )
+            {
+                _name = name;
+                _previous = System.Threading.Thread.CurrentThread.Priority;
+                System.Threading.Thread.CurrentThread.Priority = System.Threading.ThreadPriority.Highest;
+                _watch = System.Diagnostics.Stopwatch.StartNew();
+            }
+
+            public static IDisposable Run()
+            {
+                return Run( String.Empty );
+            }
+
+            public static IDisposable Run( string name )
+            {
+                return new Timed( name );
+            }
+
+            public void Dispose()
+            {
+                _watch.Stop();
+                System.Threading.Thread.CurrentThread.Priority = _previous;
+                if( String.IsNullOrEmpty( _name ) )
+                {
+                    WL( _watch.ElapsedMilliseconds );
+                }
+                else
+                {
+                    WL( "{0}: {1}", _name, _watch.ElapsedMilliseconds );
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Solution/FormRunner/FormTester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FormRunner
+{
+    public partial class FormTester : Form
+    {
+        public FormTester()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
