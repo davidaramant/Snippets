b581dcf63c10461135b69c2e1271e3bbb40a4ea0
David Aramant <aramant@sep.com>
2014-04-10 16:23:16 -0400

    Adding NUnit project

---
+++ Solution/NUnitPlayground/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnitPlayground
+{
+    [TestFixture]
+    public sealed class Tests
+    {
+        [Test]
+        public void Example()
+        {
+
+        }
+    }
+}
