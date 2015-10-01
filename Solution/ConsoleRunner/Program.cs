e6629d00d10be4a49a33f3ce39c77aa0b18c009f
David Aramant <davidaramant@gmail.com>
2015-10-01 14:24:43 -0400

    Updating to .NET 4.6

--- Solution/ConsoleRunner/Program.cs
+++ Solution/ConsoleRunner/Program.cs
@@ -3,36 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleRunner
 {
     sealed class Program
     {
-        public static void RunSnippet( string[] args )
+        public static void RunSnippet(string[] args)
         {
 
         }
 
         #region Helper methods
 
-        private static string Desktop
-        {
-            get { return Environment.GetFolderPath( Environment.SpecialFolder.DesktopDirectory ); }
-        }
+        private static string DesktopPath => Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
 
-        public static void Main( string[] args )
+        public static void Main(string[] args)
         {
             try
             {
-                RunSnippet( args );
+                RunSnippet(args);
             }
-            catch( Exception e )
+            catch (Exception e)
             {
-                Console.WriteLine( "---\nEXCEPTION:\n" + e + "\n" );
+                WL("---\nEXCEPTION:\n" + e + "\n");
             }
             finally
             {
-                Console.Write( "Press any key to continue..." );
+                Console.Write("Press any key to continue...");
                 Console.ReadKey();
             }
         }
@@ -42,15 +40,15 @@ namespace ConsoleRunner
             Console.WriteLine();
         }
 
-        private static void WL( object text, params object[] args )
+        private static void WL(object text, params object[] args)
         {
-            if( text == null )
+            if (text == null)
             {
-                Console.WriteLine( "null" );
+                Console.WriteLine("null");
             }
             else
             {
-                Console.WriteLine( text.ToString(), args );
+                Console.WriteLine(text.ToString(), args);
             }
         }
 
@@ -67,20 +65,17 @@ namespace ConsoleRunner
         sealed class Timed : IDisposable
         {
             readonly System.Diagnostics.Stopwatch _watch;
-            readonly string _name = String.Empty;
-            readonly System.Threading.ThreadPriority _previous;
+            readonly string _name;
 
             private Timed(string name)
             {
                 GC.Collect(2);
                 GC.WaitForPendingFinalizers();
                 _name = name;
-                _previous = System.Threading.Thread.CurrentThread.Priority;
-                System.Threading.Thread.CurrentThread.Priority = System.Threading.ThreadPriority.Highest;
                 _watch = System.Diagnostics.Stopwatch.StartNew();
             }
 
-            public static IDisposable Run(string name = "")
+            public static IDisposable Run(string name)
             {
                 return new Timed(name);
             }
@@ -88,17 +83,7 @@ namespace ConsoleRunner
             public void Dispose()
             {
                 _watch.Stop();
-                System.Threading.Thread.CurrentThread.Priority = _previous;
-                if (String.IsNullOrEmpty(_name))
-                {
-                    WL("{0}ms", _watch.ElapsedMilliseconds);
-                }
-                else
-                {
-                    WL("{0}: {1}ms", _name, _watch.ElapsedMilliseconds);
-                }
-                GC.Collect(2);
-                GC.WaitForPendingFinalizers();
+                WL($"{_name}: {_watch.ElapsedMilliseconds}ms");
             }
         }
 
